c65df22a6770b811766f7d46aeb544e601452a84
mjtrachta <61702195+mjtrachta@users.noreply.github.com>
2023-02-12 15:23:32 -0300

    modulo mascotas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MascotasModule } from './mascotas/mascotas.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mssql',
+      host: 'localhost',
+      port: 2471,
+      username: 'sa',
+      password: '123456',
+      database: 'HOTEL_EXTRADOS',
+      entities: ["dist/**/*.entity.js"],
+
+      extra: {trustServerCertificate:true}
+    }),
+    MascotasModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
