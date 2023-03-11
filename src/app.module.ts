835b18f2b7adf5abfa601f4980bafd6a61776406
mjtrachta <61702195+mjtrachta@users.noreply.github.com>
2023-03-10 23:18:38 -0300

    capa de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -12,32 +11,19 @@ import { MascotasModule } from './mascotas/mascotas.module';
 import { TurnosModule } from './turnos/turnos.module';
 import { DatabaseModule } from './database/database.module';
 
-
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env',
       isGlobal: true,
     }),
-    TypeOrmModule.forRoot({
-      type: 'mssql',
-      host: 'localhost',
-      port: 2471,
-      username: 'sa',
-      password: '123456',
-      database: 'MASCOTAS',
-      entities: ['dist/**/*.entity.js'],
-
-      extra: { trustServerCertificate: true },
-    }),
+    DatabaseModule,
     UsuariosModule,
     AuthModule,
     MascotasModule,
     TurnosModule,
-    DatabaseModule,
   ],
-
   controllers: [AppController],
-  providers: [AppService, JwtStrategy],
+  providers: [AppService, JwtStrategy, JwtAuthGuard],
 })
 export class AppModule {}
